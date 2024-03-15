52fede092e63dfe3d3b9c6e3db2930429f338833
Daniel Crigan <crgdaniel@yahoo.com>
2024-03-15 14:05:59 +0000

    Introduce lexicon for consistent bot communication

---
+++ lexicon/lexicon_en.py
@@ -0,0 +1,21 @@
+LEXICON_EN: dict[str, str] = {
+    "/start": '<b>Hello!</b>\nLet\'s play a game of "Rock, Paper, Scissors"?\n\n '
+    "If you forgot the rules, type /help for assistance!\n\n<b>Ready to play?</b>",
+    "/help": "This is a very simple game. We both simultaneously choose "
+    "one of three items: rock, paper, or scissors.\n\nIf our choices match "
+    "- it's a draw, otherwise rock beats scissors, scissors beat paper, "
+    "and paper beats rock.\n\n<b>Ready to play?</b>",
+    "rock": "Rock 🗿",
+    "paper": "Paper 📜",
+    "scissors": "Scissors ✂",
+    "yes_button": "Let's go!",
+    "no_button": "No, thanks!",
+    "other_answer": "Sorry, I don't understand this message...",
+    "yes": "Great! Make your choice!",
+    "no": "Too bad...\nIf you want to play later, just expand the keyboard "
+    'and press "Let\'s go!"',
+    "bot_won": "I won!\n\nWanna play again?",
+    "user_won": "You won! Congratulations!\n\nLet's play again?",
+    "nobody_won": "It's a draw!\n\nWant to continue?",
+    "bot_choice": "My choice",
+}
